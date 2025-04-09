2c76d68b0aacdb576b4450e51b3479630933abea
Connor Hall <connrhall204@gmail.com>
2025-04-09 17:04:11 -0500

    Added partial validation

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,3 +4,21 @@ const { log } = console;
 
 import * as utils from './utils.js';
 
+const loginButton = utils.select()
+const loginSection = utils.select('.log-in');
+const username = utils.select('.username');
+const password = utils.select('.password');
+
+function validUser() {
+    if (username.value != '') {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function validPass() {
+
+}
+
+utils.listen('log-in')
