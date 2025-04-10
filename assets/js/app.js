b6082510e8064dfdf869a1ebaf70a784b6ddcd63
Connor Hall <connrhall204@gmail.com>
2025-04-09 22:47:53 -0500

    finished login screen

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,10 +4,12 @@ const { log } = console;
 
 import * as utils from './utils.js';
 
-const loginButton = utils.select()
-const loginSection = utils.select('.log-in');
+const loginNav = utils.select('.login-nav');
+const loginScreen = utils.select('.login-container');
+const loginButton = utils.select('.login-button');
 const username = utils.select('.username');
 const password = utils.select('.password');
+const exit = utils.select('.exit');
 
 function validUser() {
     if (username.value != '') {
@@ -18,7 +20,23 @@ function validUser() {
 }
 
 function validPass() {
-
+    if (password.value != '') {
+        return true;
+    } else {
+        return false;
+    }
 }
 
-utils.listen('log-in')
+utils.listen('click', loginNav, () => {
+    loginScreen.classList.remove('hide');
+});
+
+utils.listen('click', exit, () => {
+    loginScreen.classList.add('hide');
+});
+
+utils.listen('click', loginButton, () => {
+   if (validUser() && validPass()) {
+    loginScreen.classList.add('hide');
+   }
+});
